8bce9e2421ca245fec1cfdca9986a3331d451c59
Ali Yahyayev <eliyahyayev04@gmail.com>
2025-07-11 07:51:01 -0700

    add areas

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,8 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
