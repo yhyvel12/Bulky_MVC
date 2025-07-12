6120ec731d2752490e5c846adafec55458ecf4b8
Ali Yahyayev <eliyahyayev04@gmail.com>
2025-07-12 01:40:55 -0700

    add product table

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Bulky.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,45 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Title",
+                    Description = "Description",
+                    ISBN = "Isbn",
+                    Author = "Author",
+                    ListPrice = 99,
+                    Price = 90,
+                    Price50 = 85,
+                    Price100 = 80
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Title2",
+                    Description = "Description2",
+                    ISBN = "Isbn2",
+                    Author = "Author2",
+                    ListPrice = 98,
+                    Price = 89,
+                    Price50 = 84,
+                    Price100 = 79
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Title3",
+                    Description = "Description3",
+                    ISBN = "Isbn3",
+                    Author = "Author3",
+                    ListPrice = 97,
+                    Price = 88,
+                    Price50 = 83,
+                    Price100 = 78
+                }
                 );
         }
     }
